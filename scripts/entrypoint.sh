e517944207b9e431cc2e08d3a838a33f154ddeb1
agladkikh <theshamuel@gmail.com>
2018-08-04 21:44:18 +0300

    fixed generate pages

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,7 @@ echo "=>Start hugo"
 #set TZ
 cp /usr/share/zoneinfo/$TZ /etc/localtime && \
 echo $TZ > /etc/timezone
-(
+
 if [ ! -f /${SITE_NAME}/content ]; then
   echo "=>Generate hugo site"
   hugo new site ${SITE_NAME}_tmp
@@ -14,17 +14,4 @@ if [ ! -f /${SITE_NAME}/content ]; then
 fi
   cd /${SITE_NAME}
 
-hugo && hugo server --watch=true
-)&
-(
-if [ ! -f /blog.${SITE_NAME}/content ]; then
-  cd ../
-  echo "=>Generate hugo site"
-  hugo new site blog.${SITE_NAME}_tmp
-  cp -vfr /blog.${SITE_NAME}_tmp/* /blog.${SITE_NAME}
-  rm -rfr /blog.${SITE_NAME}_tmp
-  echo "=>Successfully created blog"
-fi
-  cd /blog.${SITE_NAME}
-hugo && hugo server --watch=true
-)&
+hugo -v
